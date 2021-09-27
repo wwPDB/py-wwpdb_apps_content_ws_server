9bda1cf0000b9f6f2d9501d711195ccc7d4fdd6f
John Berrisford <jmb@ebi.ac.uk>
2021-09-27 08:24:25 +0100

    same queue logic for messaging queue

--- wwpdb/apps/content_ws_server/message_queue/MessageQueue.py
+++ wwpdb/apps/content_ws_server/message_queue/MessageQueue.py
@@ -1,10 +1,12 @@
-from wwpdb.utils.config.ConfigInfo import getSiteId
+from wwpdb.utils.config.ConfigInfo import getSiteId, ConfigInfo
 
 
 def get_queue_name(site_id=None):
     if not site_id:
         site_id = getSiteId()
-    queue_name = 'contentws_queue_{}'.format(site_id)
+    cI = ConfigInfo(site_id)
+    message_queue = cI.get('SITE_MESSAGE_QUEUE')
+    queue_name = message_queue if message_queue else 'contentws_queue_{}'.format(site_id)
     return queue_name
 
 
